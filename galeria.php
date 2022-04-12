691d015e5d3d04ef0b33f799f1b8d79369bea989
Enrico <enricoferreiradossantos@gmail.com>
2022-04-11 22:00:13 -0300

    update

--- galeria.php
+++ galeria.php
@@ -1,29 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php include "cabecalho.php" ?>
 
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-    <title>Clorocine</title>
-</head>
 <body>
-  <?php
-  //
-  ?>
 
   <nav class="nav-extended purple lighten-3">
       <div class="nav-wrapper">
           <ul id="nav-mobile" class="right">
-            <li><a href="galeria.php">Galeria</a></li>
+            <li class="active"><a href="galeria.php">Galeria</a></li>
             <li><a href="cadastrar.php">Cadastrar</a></li>
           </ul>
        </div>
@@ -44,7 +26,6 @@
           <div class="card hoverable">
             <div class="card-image">
               <img src="https://image.tmdb.org/t/p/w300/q6725aR8Zs4IwGMXzZT8aC8lh41.jpg">
-              <span class="card-title">Vingadores: Ultimato</span>
               <a class="btn-floating halfway-fab waves-effect waves-light red">
                   <i class="material-icons">favorite_border</i>
               </a>
@@ -53,6 +34,7 @@
                 <p class="valign-wrapper">
                     <i class="material-icons amber-text">star</i> 9,7
                 </p>
+                <span class="card-title">Vingadores: Ultimato</span>
                 <p>Após os eventos devastadores de “Vingadores: Guerra Infinita”, o universo está em ruínas devido aos esforços do Titã Louco, Thanos. Com a ajuda de aliados remanescentes, os Vingadores devem se reunir mais uma vez a fim de desfazer as ações de Thanos e restaurar a ordem no universo de uma vez por todas, não importando as consequências.</p>
             </div>
           </div>
@@ -68,10 +50,46 @@
             </div>
             <div class="card-content">
                 <p class="valign-wrapper">
-                    <i class="material-icons amber-text">star</i> 9,7
+                    <i class="material-icons amber-text">star</i> 6,1
                 </p>
                 <span class="card-title">Ad Astra - Rumo às Estrelas</span>
                 <p>Roy McBride é um engenheiro espacial, portador de um leve grau de autismo, que decide empreender a maior jornada de sua vida: viajar para o espaço, cruzar a galáxia e tentar descobrir o que aconteceu com seu pai, um astronauta que se perdeu há vinte anos atrás no caminho para Netuno.</p>
+            </div> 
+          </div>
+      </div>
+
+      <div class="col s3">
+          <div class="card hoverable">
+            <div class="card-image">
+              <img src="https://image.tmdb.org/t/p/w300/f4SvCKIUrC2cDR7Xo4k1kaGAqQ2.jpg">
+              <a class="btn-floating halfway-fab waves-effect waves-light red">
+                  <i class="material-icons">favorite_border</i>
+              </a>
+            </div>
+            <div class="card-content">
+                <p class="valign-wrapper">
+                    <i class="material-icons amber-text">star</i> 7,7
+                </p>
+                <span class="card-title">Sonic 2: O Filme</span>
+                <p>Depois de se estabelecer em Green Hills, Sonic está pronto para mais liberdade e deixar sua marca como um herói, e Tom e Maddie concordam em deixá-lo em casa enquanto vão de férias. Mas, assim que eles se foram, Dr. Robotnik volta, desta vez com um novo parceiro, Knuckles, em busca de uma esmeralda que tem o poder de construir e destruir civilizações. Sonic se une a um novo companheiro, Tails, e juntos eles embarcam em uma jornada para encontrar a esmeralda antes que ela caia nas mãos erradas.</p>
+            </div> 
+          </div>
+      </div>
+
+      <div class="col s3">
+          <div class="card hoverable">
+            <div class="card-image">
+              <img src="https://image.tmdb.org/t/p/w300/wd7b4Nv9QBHDTIjc2m7sr0IUMoh.jpg">
+              <a class="btn-floating halfway-fab waves-effect waves-light red">
+                  <i class="material-icons">favorite_border</i>
+              </a>
+            </div>
+            <div class="card-content">
+                <p class="valign-wrapper">
+                    <i class="material-icons amber-text">star</i> 7,9
+                </p>
+                <span class="card-title">Batman</span>
+                <p>Bruce Wayne é um jovem bilionário da cidade de Gotham City, Nova Jersey, que também age secretamente como o vigilante noturno Batman após o assassinato dos seus pais.</p>
             </div> 
           </div>
       </div>
